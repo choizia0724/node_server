1d00073b39438e038ec8f3bf5b17ee234018309f
choizia <st.jyeon@gmail.com>
2025-08-12 01:07:31 +0900

    change connectString

--- src/services/connectDB.js
+++ src/services/connectDB.js
@@ -6,7 +6,8 @@ export default async function connectDB() {
     const connection = await oracledb.getConnection({
       user: process.env.DB_USER, // .env 파일에서 DB 사용자 이름을 가져옵니다.
       password: process.env.DB_PASSWORD, // .env 파일에서 DB 비밀번호를 가져옵니다.
-      connectString: "pkodaz6a1r6ul85c_high",
+      connectString:
+        "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-chuncheon-1.oraclecloud.com))(connect_data=(service_name=g84857d7fd71aaa_pkodaz6a1r6ul85c_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))",
       walletLocation: "/app/wallet",
     });
 
