6523cd52de137c4fe2662c4f0215b56d89d1d3c1
choizia <st.jyeon@gmail.com>
2025-08-17 14:54:56 +0900

    add Post route

--- routes/stock.js
+++ routes/stock.js
@@ -16,6 +16,54 @@ router.get("/", async function (req, res, next) {
     });
 });
 
+/* GET stock by symbol */
+router.get("/:symbol", async function (req, res, next) {
+  const symbol = req.params.symbol;
+  models.Stock.findOne({ where: { symbol } })
+    .then((stock) => {
+      if (stock) {
+        res.status(200).json(stock);
+      } else {
+        res.status(404).send("Stock not found.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching stock data:", error);
+      res.status(500).send("Error fetching stock data.");
+    });
+});
+
+/* Get stock by param */
+router.post("/search", async function (req, res, next) {
+  const { symbol, name, mrktctg } = req.body;
+  const whereClause = {};
+
+  if (symbol) {
+    whereClause.symbol = symbol;
+  }
+  if (name) {
+    whereClause.name = { [models.Sequelize.Op.like]: `%${name}%` };
+  }
+  if (mrktctg) {
+    whereClause.mrktctg = mrktctg;
+  }
+
+  // If no search parameters are provided, return all stocks
+  if (Object.keys(whereClause).length === 0) {
+    return res.status(400).send("No search parameters provided.");
+  }
+
+  models.Stock.findAll({ where: whereClause })
+    .then((stocks) => {
+      res.status(200).json(stocks);
+    })
+    .catch((error) => {
+      console.error("Error searching stock data:", error);
+      res.status(500).send("Error searching stock data.");
+    });
+});
+
+/* Fetch and update stock data */
 router.get("/getStockData", async function (req, res, next) {
   try {
     await getStockData();
