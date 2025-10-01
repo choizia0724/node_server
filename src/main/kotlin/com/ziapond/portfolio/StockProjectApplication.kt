eb249e503d2ba76342422067977c745a3c0f4629
choizia <st.jyeon@gmail.com>
2025-10-01 12:52:00 +0900

    change java to kotlin

---
+++ src/main/kotlin/com/ziapond/portfolio/StockProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.ziapond.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/ziapond/portfolio/StockProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ziapond.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
